86a3174b7841e5e3540d99d431dcbf5a00739c7f
Stefan Andres <s.andres@syseleven.de>
2015-09-28 11:40:57 +0200

    os-852 fix timeout handling

--- files/check_instance.d/generic.sh
+++ files/check_instance.d/generic.sh
@@ -75,7 +75,11 @@ spawn_vm() {
 
   # Wait for stack status to change from CREATE_IN_PROGRESS to CREATE_{COMPLETE,FAILED}
   # sometimes watch never finishes
-  timeout 30 watch -g heat stack-show ${stack_id} \| grep CREATE_ > /dev/null 2>&1
+  if ! timeout 30 watch -g heat stack-show ${stack_id} \| grep CREATE_ > /dev/null 2>&1; then
+    echo 'CRITICAL: stack creation did not finish in 30s'
+    heat stack-show $stack_id
+    return 2
+  fi
 
   if heat stack-show ${stack_id} | grep CREATE_COMPLETE > /dev/null; then
     return 0
